51572ff18500ec65a7ae97ece29af520ace88759
Falokut <71385842+Falokut@users.noreply.github.com>
2023-12-14 20:51:10 +0300

    span grpc error codes improvement

--- internal/service/service.go
+++ internal/service/service.go
@@ -6,9 +6,9 @@ import (
 
 	"github.com/Falokut/casts_service/internal/repository"
 	casts_service "github.com/Falokut/casts_service/pkg/casts_service/v1/protos"
-	"github.com/Falokut/grpc_errors"
 	"github.com/opentracing/opentracing-go"
 	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc/codes"
 )
 
 type CastsService struct {
@@ -27,22 +27,18 @@ func NewCastsService(logger *logrus.Logger, repoManager repository.Manager) *Cas
 	}
 }
 
-
-func (s *CastsService) GetCast(ctx context.Context, in *casts_service.GetCastRequest) (*casts_service.Cast,error){
-	span, ctx:= opentracing.StartSpanFromContext(ctx,"CastsService.GetCast")
+func (s *CastsService) GetCast(ctx context.Context, in *casts_service.GetCastRequest) (*casts_service.Cast, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "CastsService.GetCast")
 	defer span.Finish()
 
-	var err error
-	defer span.SetTag("grpc.status",grpc_errors.GetGrpcCode(err))
-
-
-	cast, err := s.repoManager.GetCast(ctx,in.CastID)
-	if errors.Is(err,repository.ErrNotFound) {
-		return nil, s.errorHandler.createErrorResponce(ErrNotFound,err.Error())
+	cast, err := s.repoManager.GetCast(ctx, in.CastID)
+	if errors.Is(err, repository.ErrNotFound) {
+		return nil, s.errorHandler.createErrorResponceWithSpan(span, ErrNotFound, err.Error())
 	}
 	if err != nil {
-		return nil, s.errorHandler.createErrorResponce(ErrInternal,err.Error())
+		return nil, s.errorHandler.createErrorResponceWithSpan(span, ErrInternal, err.Error())
 	}
 
-	return &casts_service.Cast{Actors: cast.Actors},nil
+	span.SetTag("grpc.status", codes.OK)
+	return &casts_service.Cast{Actors: cast.Actors}, nil
 }
